9039423263f20db514ac4278ee026d62502fc58b
Alessandro-Salerno <67583725+tzyvoski@users.noreply.github.com>
2022-07-06 20:10:40 +0200

    Added build command

--- src/CBuild/__main__.py
+++ src/CBuild/__main__.py
@@ -1,20 +1,23 @@
 import json
 import sys
+import os
 
 
 class CBuildConfiguration:
     def __init__(self, config: dict):
-        self._config       = config
-        self._dir_config   = config["directories"]
-        self._build_config = config["build"]
+        self._config        = config
+        self._dir_config    = config["directories"]
+        self._build_config  = config["build"]
 
-        self.source_dirs   = self._dir_config["source"]
-        self.include_dirs  = self._dir_config["include"]
-        self.out_dir       = self._dir_config["output"]
+        self.source_dirs    = self._dir_config["source"]
+        self.include_dirs   = self._dir_config["include"]
+        self.out_dir        = self._dir_config["output"]
 
-        self.project_name  = self._build_config["name"]
-        self.release_mode  = self._build_config["release"]
-        self.output_format = self._build_config["format"]
+        self.compiler_path  = self._build_config["compiler"]
+        self.file_extension = self._build_config["file extension"]
+        self.project_name   = self._build_config["name"]
+        self.release_mode   = self._build_config["release"]
+        self.output_format  = self._build_config["format"]
 
 
 class CBuildConfigurationException(Exception):
@@ -34,13 +37,29 @@ def read_config() -> CBuildConfiguration:
         raise e
 
 
+def build_command(config: CBuildConfiguration) -> str:
+    command = f"{config.compiler_path} -Wall -Wextra -I. {'-O3' if config.release_mode else '-g '}"
+
+    for incdir in config.include_dirs:
+        command += f"-I {incdir}"
+
+    for srcdir in config.source_dirs:
+        files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(srcdir) for f in filenames if os.path.splitext(f)[1] == config.file_extension]
+        for file in files:
+            command += f"{file} "
+
+    command += f"-o {config.out_dir}/{config.project_name}"
+    return command
+
+
 def main(argv: list) -> int:
     # Build
     if len(argv) == 1:
         config = read_config()
+        os.system(build_command(config))
         return 0
     
-    return 0
+    return -1
 
 
 if __name__ == "__main__":
