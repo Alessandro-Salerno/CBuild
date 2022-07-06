b195873d62c2679227d5a83e11d720010341dbcd
Alessandro-Salerno <67583725+tzyvoski@users.noreply.github.com>
2022-07-06 19:35:41 +0200

    Added Configuration

--- src/CBuild/__main__.py
+++ src/CBuild/__main__.py
@@ -2,8 +2,45 @@ import json
 import sys
 
 
+class CBuildConfiguration:
+    def __init__(self, config: dict):
+        self._config       = config
+        self._dir_config   = config["directories"]
+        self._build_config = config["build"]
+
+        self.source_dirs   = self._dir_config["source"]
+        self.include_dirs  = self._dir_config["include"]
+        self.obj_dir       = self._dir_config["objects"]
+        self.out_dir       = self._dir_config["output"]
+
+        self.project_name  = self._build_config["name"]
+        self.release_mode  = self._build_config["release"]
+        self.output_format = self._build_config["format"]
+
+
+class CBuildConfigurationException(Exception):
+    pass
+
+
+def read_config() -> CBuildConfiguration:
+    try:
+        with open("./CBuild.json", "r") as config_file:
+            json_content = config_file.read()
+            parsed_json  = json.loads(json_content)
+
+            return CBuildConfiguration(parsed_json)
+    except FileNotFoundError as e:
+        raise CBuildConfigurationException("CBuild Configuration file not found.")
+    except Exception as e:
+        raise e
+
+
 def main(argv: list) -> int:
-    print(argv)
+    # Build
+    if len(argv) == 1:
+        config = read_config()
+        return 0
+    
     return 0
 
 
